c10e9a2f17c8aa550e499dc11573e582ff135821
Aman <amanverma5837@gmail.com>
2024-07-03 02:56:44 +0530

    Added 3 More Apps For Topic, Offset And Core

--- topic/apps.py
+++ topic/apps.py
@@ -2,6 +2,6 @@
 from django.apps import AppConfig
 
 
-class InternalConfig(AppConfig):
+class TopicConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "internal"
+    name = "topic"
